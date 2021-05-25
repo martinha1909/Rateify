567a78891cd1dfafc51b1e1757b5d94e9d31f4d8
Kep <directorkai@gmail.com>
2021-05-25 16:51:53 -0600

    first artist change

--- frontend/artist.php
+++ frontend/artist.php
@@ -16,25 +16,20 @@
     <!--Inter UI font-->
     <link href="https://rsms.me/inter/inter-ui.css" rel="stylesheet">
 
-    <!--vendors styles-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
-
     <!-- Bootstrap CSS / Color Scheme -->
     <link rel="stylesheet" href="css/default.css" id="theme-color">
 </head>
-<body>
+<body class="bg-dark">
 
 <!--navigation-->
 <section class="smart-scroll">
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-md navbar-dark">
-            <a class="navbar-brand heading-black" href="index.php">
-                Rateify
+    <div class="container-xxl">
+        <nav class="navbar navbar-expand-md navbar-dark bg-orange">
+            <a id = "href-hover" class="navbar-brand heading-black" href="#">
+                HASSNER
             </a>
-            <p style = "position: absolute;right:0px; top:0px;" class="navbar-light bg-dark">Shares Distributed</p>
-            <p style = "position: absolute;right:70px; top:26px;">
+            <p class="navbar-light bg-dark">Shares Distributed</p>
+            <p>
                 <?php
                     include '../APIs/logic.php';
                     include '../APIs/connection.php';
@@ -44,14 +39,14 @@
                     echo $_SESSION['artist_distributed']['Share_Distributed'];
                 ?>
             </p>
-            <div  style = "position: absolute;right:40px; top:25px;" class="col text-right">
+            <div class="col text-right">
                 <a href="../APIs/IncreaseSharesDistributed.php" onclick='window.location.reload();'>+</a>
             </div>
-            <div  style = "position: absolute;right:75px; top:25px;" class="col text-right">
+            <div class="col text-right">
                 <a href="../APIs/DecreaseSharesDistributed.php" onclick='window.location.reload();'>-</a>
             </div>
-            <p style = "position: absolute;right:165px; top:0px;" class="navbar-light bg-dark">Available Shares</p>
-            <p style = "position: absolute;right:230px; top:26px;">
+            <p class="navbar-light bg-dark">Available Shares</p>
+            <p>
                 <?php
                    $result2 = searchArtistShares($conn, $_SESSION['username']);
                    $artist_share = $result2->fetch_assoc();
@@ -70,53 +65,47 @@
 </section>
 
 <!-- listener functionality -->
-<section class="py-7 py-md-0 bg-hero" id="login">
-    <div class="container">
-        <div class="row vh-md-100">
+<section id="login">
+    <div class="container-fluid">
+        <div class="row">
             <div class="col-12 mx-auto my-auto text-center">
-              
-              <div class="col text-center">
-              <h1> Hello <?php echo $_SESSION['username'] ?></h1>
-              <p> Username: <?php echo $_SESSION['username'] ?></p> 
-              <p> Account Type: <?php echo $_SESSION['account_type'] ?></p> 
-              </div>
 
               <!-- header -->
-              <div class="col text-center">
-                <h2> Please select an action. </h2>
-              </div>
 
               <div class="col-md-8 col-12 mx-auto pt-5 text-center">
                 <form action="../APIs/DisplayArtistSongsConnection.php" method="post">
-                  <input type = "submit" class="btn btn-primary" role="button" aria-pressed="true" value = "My Songs">
+                  <input type = "submit" class="btn btn-primary" role="button" aria-pressed="true" value = "Songs">
                 </form>
               </div>
 
               <div class="col-md-8 col-12 mx-auto pt-5 text-center">
                 <form action="../APIs/DisplayArtistAlbumsConnection.php" method="post">
-                  <input type = "submit" class="btn btn-primary" role="button" aria-pressed="true" value = "My Albums">
+                  <input type = "submit" class="btn btn-primary" role="button" aria-pressed="true" value = "Albums/EP">
                 </form>
               </div>
 
               <div class="col-md-8 col-12 mx-auto pt-5 text-center">
                 <a href="CreateSongView.php" class="btn btn-primary" role="button" aria-pressed="true">
-                  Create a new Song
+                  Add Song(s)
                 </a>
               </div>
 
               <div class="col-md-8 col-12 mx-auto pt-5 text-center">
                 <a href="CreateAlbumView.php" class="btn btn-primary" role="button" aria-pressed="true">
-                  Create Albums
+                  Add Album(s)
                 </a>
               </div>
 
               <!-- logout button-->
               <div class="col-md-8 col-12 mx-auto pt-5 text-center">
                 <a href="ArtistWritePost.php" class="btn btn-primary" role="button" aria-pressed="true">
-                  Write a post
+                  Add post(s)
                 </a>
               </div>
-              
+
+              <div class="col-md-8 col-12 mx-auto pt-5 text-center">
+              <a class="dropdown-item" id="dashboard-hover" style="background-color: transparent;" href="login.php">Log out</a>
+              </div>
             </div>
         </div>
     </div>
